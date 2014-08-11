7fca003a17a33138ffcb9f22f6b0019f4194927d
Kanikar <kphan@kbb.com>
2014-08-11 08:03:58 -0700

    update to version 0.0.4

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
-# Stats
+# CSS Stats
 
-Stats utilize [Stylestats](https://github.com/t32k/stylestats) and [CSSCSS](https://github.com/zmoazeni/csscss) to parse a css file and gives you stats on it.
+CSS Stats utilize [Stylestats](https://github.com/t32k/stylestats) and [CSSCSS](https://github.com/zmoazeni/csscss) to parse a css file and gives you stats on it.
 
 ## Getting Started
 
-Stats needs the following installed: Grunt CLI `~0.4.0` and Ruby (`v1.9.x` and up).
-
-If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to Grunt CLI.
-
-To check Ruby is installed on your machine use `ruby -v`. 
-[RubyInstaller](http://rubyinstaller.org/) is a Windows-based installer, the easiest way to get Ruby on Windows.
+CSS Stats require Ruby (`v1.9.x` and up) installed. OSX should have Ruby preinstalled. For Windows you can use [RubyInstaller](http://rubyinstaller.org/). This is a Windows-based installer, the easiest way to get Ruby on Windows. To check if Ruby is installed on your machine use `ruby -v`. 
 
 Usage
 ```
-Usage: stats [--stylestats|--csscss] [css]
+Usage: css-stats [--stylestats|--csscss] [css]
 
 Commands:
 
-setup [type]
-   run setup command to install parser
+setup 
+   install the necessary css parser engines
+
+start
+   generate css-stats scaffolding to parse css
 
 Options:
 
@@ -26,5 +24,4 @@ Options:
 -V, --version           output the version number
 -s, --stylestats [css]  parse css for overall stats
 -c, --csscss [css]      parse css for rulesets that have duplicated declarations
--t, --type [parser]     parser to install [stylestats|csscss]
 ```
--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var parsing = [
 ];
 
 cssStats.stylestats = function(css, path) {
+
   var stats = new StyleStats(css, './lib/default.json');
 
   var ui = new inquirer.ui.BottomBar();
@@ -51,7 +52,7 @@ cssStats.csscss = function(css, path) {
   var confirm = [{
     type: 'confirm',
     name: 'confirm',
-    message: 'This may take a while depending on your CSS size, would you like to continue?'.yellow,
+    message: 'This may take a while depending on your CSS file size, would you like to continue?'.yellow,
     default: false
   }];
 
@@ -67,9 +68,7 @@ cssStats.csscss = function(css, path) {
 
       var target = '--target=' + css;
 
-      var test = '--test=' + path;
-
-      var cmd = spawn(cmdify('grunt'), [ 'duplicate', target, test ], { stdio: 'pipe' });
+      var cmd = spawn(cmdify('grunt'), [ 'duplicate', target ], { stdio: 'pipe' });
 
       cmd.stdout.pipe(ui.log);
 
@@ -82,62 +81,89 @@ cssStats.csscss = function(css, path) {
   });
 };
 
-cssStats.setup = function(type) {
-  if (type === undefined) {
-    console.log();
-    console.log("  error: option '-t, --type [parser]' argument missing".red);
-    console.log();
-  } else {
-    switch (program.type) {
-      case 'stylestats':
-        console.log('Installing...'.cyan);
-        var ui = new inquirer.ui.BottomBar();
+cssStats.setup = function() {
+  console.log('Installing css-stats parser...'.cyan + '\nenter your computer password if prompted'.underline);
 
-        var cmd = spawn(cmdify('npm'), [ '-g', 'install', 'stylestats' ], { stdio: 'pipe' });
+  var ui = new inquirer.ui.BottomBar();
 
-        cmd.stdout.pipe(ui.log);
+  var cmd = spawn(cmdify('sudo'), ['gem', 'install', 'csscss' ], { stdio: 'pipe' });
 
-        cmd.on('close', function() {
-          ui.updateBottomBar('Successfully installed stylestats'.green);
-          process.exit();
-        });
-      break;
-      case 'csscss':
-        console.log('Installing... (enter your computer password if prompted)'.cyan);
-        var ui = new inquirer.ui.BottomBar();
+  cmd.stdout.pipe(ui.log);
 
-        var cmd = spawn(cmdify('sudo'), [ 'gem', 'install', 'csscss' ], { stdio: 'pipe' });
-
-        cmd.stdout.pipe(ui.log);
-
-
-        cmd.on('close', function() {
-          // clearInterval(timer);
-          ui.updateBottomBar('Successfully installed stylestats'.green);
-          process.exit();
-        });
-      break;
-      default:
-        console.log();
-        console.log("  error: option '-t, --type [parser]' incorrect argument".red);
-        console.log();
-    }
-  }
+  cmd.on('close', function() {
+    ui.updateBottomBar('Installed successfully!'.green);
+    process.exit();
+  });
 };
 
-cssStats.cmd
+cssStats.start = function() {
+  var src = __dirname + '/Gruntfile.js',
+      dist = process.cwd() + '/Gruntfile.js';
+
+  fs.createReadStream(src).pipe(fs.createWriteStream(dist));
+
+  console.log('Generating css-stats scaffolding...'.cyan);
+
+  cssStats.gruntcli();
+};
+
+cssStats.gruntcli = function() {
+  var ui = new inquirer.ui.BottomBar();
+
+  var cmd = spawn(cmdify('npm'), ['install', '-g', 'grunt-cli' ], { stdio: 'pipe' });
+
+  cmd.stdout.pipe(ui.log);
+
+  cmd.on('close', function() {
+    ui.updateBottomBar('');
+    cssStats.grunt();
+  });
+};
+
+cssStats.grunt = function() {
+  var ui = new inquirer.ui.BottomBar();
+
+  var cmd = spawn(cmdify('npm'), ['install', 'grunt' ], { stdio: 'pipe' });
+
+  cmd.stdout.pipe(ui.log);
+
+  cmd.on('close', function() {
+    ui.updateBottomBar('');
+    cssStats.gruntcsscss();
+  });
+};
+
+cssStats.gruntcsscss = function() {
+  var ui = new inquirer.ui.BottomBar();
+
+  var cmd = spawn(cmdify('npm'), ['install', 'grunt-csscss' ], { stdio: 'pipe' });
+
+  cmd.stdout.pipe(ui.log);
+
+  cmd.on('close', function() {
+    ui.updateBottomBar('Ready to start!'.green);
+    process.exit();
+  });
+};
+
 program
   .version('0.0.1')
   .usage('[--stylestats|--csscss] [css]')
   .option('-s, --stylestats [css]', 'parse css for overall stats')
   .option('-c, --csscss [css]', 'parse css for rulesets that have duplicated declarations')
-  .option('-t, --type [parser]', 'parser to install [stylestats|csscss]')
 
 program
-  .command('setup [type]')
-  .description('run setup command to install parser')
+  .command('setup')
+  .description('install the necessary css parser engines')
   .action(function() {
-    cssStats.setup(program.type);
+    cssStats.setup();
+  });
+
+program
+  .command('start')
+  .description('generate css-stats scaffolding to parse css')
+  .action(function() {
+    cssStats.start();
   });
 
 program.parse(process.argv);
